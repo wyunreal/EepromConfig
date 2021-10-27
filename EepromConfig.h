6f539d5dc67c656db14b043d38af30f3ff7accd8
Wiljan Arias Milian <wyunreal@gmail.com>
2021-10-28 01:33:00 +0200

    adding collection utility

--- EepromConfig.h
+++ EepromConfig.h
@@ -15,7 +15,7 @@ template <class T>
 class EepromConfigReader
 {
 public:
-    EepromConfigReader(int configVersion, long configLocation) : config()
+    EepromConfigReader(int configVersion, long configLocation)
     {
         codeVersion = configVersion;
         startAddress = configLocation;
@@ -49,6 +49,122 @@ private:
     ConfigData<T> config;
     long startAddress;
     int codeVersion;
+};
+
+template <class T>
+struct SetItem
+{
+    byte isPresent;
+    T item;
+};
+
+template <class T>
+class EepromCollectionReader;
+
+template <class T>
+class EepromCollectionIterator
+{
+    public:
+    bool next()
+    {
+        for (int i = currentIndex + 1; i < reader->maxSize; i++) {
+            currentItem = reader->getItemAtIndex(i);
+            if (currentItem.isPresent) {
+                currentIndex = i;
+                return true;
+            }
+        }
+        currentIndex = reader->maxSize;
+        return false;
+    }
+
+    T current()
+    {
+        return currentItem.item;
+    }
+
+    private:
+    SetItem<T> currentItem;
+    int currentIndex;
+    EepromCollectionReader<T>* reader;
+
+    friend class EepromCollectionReader<T>;
+};
+
+template <class T>
+class EepromCollectionReader
+{
+public:
+    EepromCollectionReader(byte configVersion, long collectionLocation, int collectionMaxSize)
+    {
+        startAddress = collectionLocation;
+        maxSize = collectionMaxSize;
+        
+        byte readVersion = EEPROM.get(startAddress, readVersion);
+        if (readVersion != configVersion) {
+            SetItem<T> slotItem;
+            slotItem.isPresent = 0;
+            EEPROM.put(startAddress, configVersion);
+            for (int slot = 0; slot < maxSize; slot++) {
+                EEPROM.put(startAddress + sizeof(byte) + sizeof(slotItem) * slot, slotItem);
+            }
+        }
+    }
+    bool addItem(T item)
+    {
+        SetItem<T> slotItem;
+        for (int slot = 0; slot < maxSize; slot++) {
+            EEPROM.get(startAddress + sizeof(byte) + sizeof(slotItem) * slot, slotItem);
+            if (!slotItem.isPresent) {
+                slotItem.item = item;
+                slotItem.isPresent = 1;
+                EEPROM.put(startAddress + sizeof(byte) + sizeof(slotItem) * slot, slotItem);
+                return true;
+            }
+        }
+        return false;
+    }
+    void setItemAtIndex(T item, int index)
+    {
+        SetItem<T> slotItem;
+        slotItem.isPresent = 1;
+        slotItem.item = item;
+        EEPROM.put(startAddress + sizeof(byte) + sizeof(slotItem) * index, slotItem);
+    }
+    bool removeItem(T item)
+    {
+        SetItem<T> slotItem;
+        for (int slot = 0; slot < maxSize; slot++) {
+            EEPROM.get(startAddress + sizeof(byte) + sizeof(slotItem) * slot, slotItem);
+            if (slotItem.isPresent && slotItem.item == item) {
+                slotItem.isPresent = 0;
+                EEPROM.put(startAddress + sizeof(byte) + sizeof(slotItem) * slot, slotItem);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    SetItem<T> getItemAtIndex(int index)
+    {
+        SetItem<T> slotItem; 
+        EEPROM.get(startAddress + sizeof(byte) + sizeof(slotItem) * index, slotItem);
+        return slotItem;
+    }
+
+    EepromCollectionIterator<T> *iterate()
+    {
+        iterator.currentIndex = -1;
+        iterator.reader = this;
+        return &iterator;
+    }
+
+    friend class EepromCollectionIterator<T>;
+
+private:
+    long startAddress;
+    int maxSize;
+    EepromCollectionIterator<T> iterator;
 };
 
 #endif
